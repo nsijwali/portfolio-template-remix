b8745855db7348df77adb75a21bdfd4c5257bd6f
Nitin Sijwali <NITIN.SIJWALI@questdot.io>
2024-03-21 00:06:46 +0530

    ui added

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,36 +5,26 @@ import { Box } from '../src/components/Styles';
 import WorkPanel from '../src/screens/WorkPanel';
 
 export const loader = async () => {
-	try {
-		const response = await fetch(`http://localhost:3000/api/getUserDetails`);
+	const response = await fetch(`http://localhost:3000/api/getUserDetails`);
 
-		const userData = await response.json();
-		return {
-			status: 200,
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			data: userData,
-		};
-	} catch (e) {
-		return {
-			status: 400,
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			data: [],
-		};
-	}
+	const userData = await response.json();
+	return {
+		status: 200,
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		data: userData,
+	};
 };
 
 const Index = () => {
 	return (
 		<>
-			<Outlet />
 			<Box>
 				<Header />
 				<WorkPanel />
 			</Box>
+			<Outlet />
 		</>
 	);
 };
