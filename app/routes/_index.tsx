e3805e5e0b6e419e323453e387d9ecfcb51d69f6
Nitin Sijwali <NITIN.SIJWALI@questdot.io>
2024-03-24 15:55:50 +0530

    ui added

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import pkg from '@remix-run/node';
+const { Request } = pkg;
 import Header from '../src/components/Header';
 import { Outlet } from '@remix-run/react';
 import { Box } from '../src/components/Styles';
 import WorkPanel from '../src/screens/WorkPanel';
 
 export const loader = async () => {
-	const response = await fetch(`/api/getUserDetails`);
+	const isProduction = process?.env?.NODE_ENV === 'production';
 
+	console.log('env-->', process?.env?.NODE_ENV);
+	// Set the base URL based on the environment
+	const baseURL = isProduction
+		? 'https://portforlio-three.vercel.app'
+		: 'http://localhost:3000';
+
+	// Construct the URL for the Express API endpoint
+	const apiUrl = `${baseURL}/api/getUserDetails`;
+
+	// Fetch data from the Express API endpoint
+	const response = await fetch(apiUrl);
+
+	// Handle response based on status
+	if (!response.ok) {
+		throw new Error('Failed to fetch data from the API');
+	}
 	const userData = await response.json();
 	return {
 		status: 200,
