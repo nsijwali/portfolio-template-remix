a80a13983cdb64a7caa0c2c097aeb977569b4db5
Nitin Sijwali <NITIN.SIJWALI@questdot.io>
2024-03-24 19:53:18 +0530

    ui added

--- remix.config.js
+++ remix.config.js
@@ -4,7 +4,6 @@ module.exports = {
 	// appDirectory: 'app',
 	// assetsBuildDirectory: 'public/build',
 	// publicPath: '/build/',
-	serverBuildDirectory: 'build',
 	// devServerPort: 8002,
 	ignoredRouteFiles: ['.*'],
 	future: {
