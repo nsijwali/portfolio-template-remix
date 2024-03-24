8e3b81b772a520c4c0e4c186e80f6b9d98758a46
Nitin Sijwali <NITIN.SIJWALI@questdot.io>
2024-03-25 00:07:52 +0530

    added change in remix config

--- app/root.tsx
+++ app/root.tsx
@@ -23,6 +23,32 @@ export const meta = () => [
 	},
 ];
 
+export const loader = async () => {
+	const isProduction = process?.env?.NODE_ENV === 'production';
+
+	// Set the base URL based on the environment
+	const baseURL = isProduction ? '' : 'http://localhost:3000';
+
+	// Construct the URL for the Express API endpoint
+	const apiUrl = `${baseURL}/api/getUserDetails`;
+
+	// Fetch data from the Express API endpoint
+	const response = await fetch(apiUrl);
+
+	// Handle response based on status
+	if (!response.ok) {
+		throw new Error('Failed to fetch data from the API');
+	}
+	const userData = await response.json();
+	console.log('data-->', response);
+	return {
+		status: 200,
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		data: userData,
+	};
+};
 export function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang='en'>
