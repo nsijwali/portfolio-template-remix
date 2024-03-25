2a01097d979b33ddfca3502595ae9a9e1adf109c
Nitin Sijwali <NITIN.SIJWALI@questdot.io>
2024-03-25 23:25:26 +0530

    added change in remix config

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,37 +1,36 @@
 import React from 'react';
 import Header from '../src/components/Header';
-// import { Outlet } from '@remix-run/react';
+import { useFetcher } from '@remix-run/react';
 import { Box } from '../src/components/Styles';
 import WorkPanel from '../src/screens/WorkPanel';
 
-export const loader = async () => {
-	const isProduction = process?.env?.NODE_ENV === 'production';
+// export const loader = async () => {
+// 	const isProduction = process?.env?.NODE_ENV === 'production';
 
-	// Set the base URL based on the environment
-	const baseURL = isProduction
-		? 'https://portforlio-three.vercel.app'
-		: 'http://localhost:3000';
+// 	// Set the base URL based on the environment
+// 	const baseURL = isProduction
+// 		? 'https://portforlio-three.vercel.app'
+// 		: 'http://localhost:3000';
 
-	// Construct the URL for the Express API endpoint
-	const apiUrl = `${baseURL}/api/getUserDetails`;
+// 	// Construct the URL for the Express API endpoint
+// 	const apiUrl = `${baseURL}/api/getUserDetails`;
 
-	// Fetch data from the Express API endpoint
-	const response = await fetch(apiUrl);
+// 	// Fetch data from the Express API endpoint
+// 	const response = await fetch(apiUrl);
 
-	// Handle response based on status
-	if (!response.ok) {
-		throw new Error('Failed to fetch data from the API');
-	}
-	const userData = await response.json();
-	console.log('data-->', response);
-	return {
-		status: 200,
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		data: userData,
-	};
-};
+// 	// Handle response based on status
+// 	if (!response.ok) {
+// 		throw new Error('Failed to fetch data from the API');
+// 	}
+// 	const userData = await response.json();
+// 	return {
+// 		status: 200,
+// 		headers: {
+// 			'Content-Type': 'application/json',
+// 		},
+// 		data: userData,
+// 	};
+// };
 
 const Index = () => {
 	return (
