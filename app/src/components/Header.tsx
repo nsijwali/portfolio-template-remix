d32d988bc45e9320f5989ca7c1403378a09e5bd6
Nitin Sijwali <NITIN.SIJWALI@questdot.io>
2024-04-14 01:34:21 +0530

    css added

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -7,8 +7,8 @@ const Header = () => {
 	return (
 		<StyledHeader className='flex w-full justify-between p-4 sm:pt-6 sm:pl-12 sm:pr-12 fixed top-0 left-0 right-0 z-10'>
 			<span>
-				<p className='text-lg font-medium'>{data?.name}</p>
-				<span>{data?.designation}</span>
+				<p className='text-xl font-medium'>{data?.name}</p>
+				<span className='text-xs text-gray-400'>{data?.designation}</span>
 			</span>
 			<span>
 				<span>LinkedIn</span>
