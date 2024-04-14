44d16c67191367347f3c4efcbb64e4d21fdfb8ca
Nitin Sijwali <NITIN.SIJWALI@questdot.io>
2024-04-14 13:46:24 +0530

    added logos

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,17 +1,20 @@
 import { Link, useLoaderData } from '@remix-run/react';
 import React from 'react';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import LogoRenderer from './LogoRenderer';
 import { StyledHeader } from './Styles';
 
 const Header = () => {
 	const { data } = useLoaderData() || [];
 	return (
 		<StyledHeader className='flex w-full justify-between p-4 sm:pt-6 sm:pl-12 sm:pr-12 fixed top-0 left-0 right-0 z-10'>
-			<span>
-				{/* <img src='/images/hd-logo.webp' /> */}
-				<p className='text-xl font-medium'>{data?.name}</p>
-				<span className='text-xs text-gray-400'>{data?.designation}</span>
-			</span>
+			<div className='flex gap-1 items-center'>
+				<LogoRenderer />
+				<div className='hidden sm:flex flex-col'>
+					<p className='text-xl font-medium'>{data?.name}</p>
+					<span className='text-xs text-gray-400'>{data?.designation}</span>
+				</div>
+			</div>
 			<span className='flex gap-1'>
 				<Link target='_blank' to={data.gitHubLink}>
 					<FaGithub size='24' />
---
+++ app/src/components/LogoRenderer.tsx
@@ -0,0 +1,27 @@
+import { useLoaderData } from '@remix-run/react';
+import React, { useState } from 'react';
+
+const LogoRenderer = () => {
+	const { data } = useLoaderData() || [];
+	const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+	const handleClick = () => {
+		const nextIndex = (currentImageIndex + 1) % data.images.length;
+		setCurrentImageIndex(nextIndex);
+	};
+	return (
+		<div
+			className='cursor-pointer w-fit'
+			title='click me'
+			onClick={handleClick}
+		>
+			<img
+				src={data.images[currentImageIndex]}
+				className='h-12 w-12 sm:h-14 sm:w-14 rounded-full border-2 border-gray-200 object-cover'
+				alt='logo'
+			/>
+		</div>
+	);
+};
+
+export default LogoRenderer;
