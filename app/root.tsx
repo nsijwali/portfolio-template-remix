374f5214cc7e57e8313aaa82161537324376aab8
Nitin Sijwali <NITIN.SIJWALI@questdot.io>
2024-04-14 12:41:09 +0530

    added favicon

--- app/root.tsx
+++ app/root.tsx
@@ -49,6 +49,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
 				<meta charSet='utf-8' />
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<Meta />
+				<meta
+					name='google-site-verification'
+					content='5iLsrNe4TgPQyAf_aeCM54KbmvB5oJJ0Xz500yb-5p8'
+				/>
 				<Links />
 			</head>
 			<body
