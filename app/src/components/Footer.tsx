c507a5636f35e9ab43454a999eed44bad7277310
Nitin Sijwali <NITIN.SIJWALI@questdot.io>
2024-04-19 11:12:06 +0530

    added resume link

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -1,20 +1,16 @@
-import { Link, useLoaderData, useNavigate } from '@remix-run/react';
+import { Link, useLoaderData } from '@remix-run/react';
 import { WiDirectionUpRight } from 'react-icons/wi';
 import React, { useMemo } from 'react';
+import pdfUrl from '~/db/nitin_sijwali_resume.pdf';
 import { StyledFooter } from './Styles';
 
 export const Footer = () => {
-	const navigate = useNavigate();
 	const { data } = useLoaderData() || [];
 	const year = useMemo(() => {
 		const d = new Date();
 		return d.getFullYear();
 	}, []);
 
-	const handleRoute = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-		navigate('/resume');
-		e.preventDefault();
-	};
 	return (
 		<StyledFooter className='relative divide-y flex justify-center pt-16 pb-6 sm:pb-10 px-2 sm:pl-12 sm:pr-12'>
 			<div className='flex flex-col gap-4 w-full'>
@@ -26,18 +22,26 @@ export const Footer = () => {
 					/>
 					<div className='flex justify-between items-center'>
 						<div className='flex flex-col text-sm'>
-							<Link target='_blank' to={data.linkedInLink} className='flex'>
+							<Link
+								target='_blank'
+								to={data.linkedInLink}
+								className='flex items-center hover:text-yellow-300'
+							>
 								Linkedin
 								<WiDirectionUpRight size='30' />
 							</Link>
-							<Link className='flex' target='_blank' to={data.gitHubLink}>
+							<Link
+								className='flex items-center hover:text-yellow-300'
+								target='_blank'
+								to={data.gitHubLink}
+							>
 								Git
 								<WiDirectionUpRight size='30' />
 							</Link>
 						</div>
 						<div className='flex flex-col text-sm'>
 							<Link
-								className='flex'
+								className='flex items-center hover:text-yellow-300'
 								reloadDocument={false}
 								target='_blank'
 								to={`https://mail.google.com/mail/?view=cm&fs=1&to=${data.email}`}
@@ -45,10 +49,15 @@ export const Footer = () => {
 								Mail
 								<WiDirectionUpRight size='30' />
 							</Link>
-							<div className='flex' onClick={handleRoute}>
+							<Link
+								className='flex items-center hover:text-yellow-300'
+								reloadDocument={false}
+								target='_blank'
+								to={pdfUrl}
+							>
 								Resume
 								<WiDirectionUpRight size='30' />
-							</div>
+							</Link>
 						</div>
 					</div>
 				</div>
--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -18,19 +18,25 @@ const Header = () => {
 		<StyledHeader className='flex w-full justify-between p-4 sm:pt-6 sm:pl-12 sm:pr-12 fixed top-0 left-0 right-0 z-10  items-center'>
 			<div className='flex gap-1 items-center' onClick={handleRoute}>
 				<LogoRenderer />
-				<div className='hidden sm:flex flex-col left-28 absolute'>
-					<p className='text-xl font-medium animate animate-fade-in-out'>
-						{data.name || ''}
-					</p>
+				<div className='hidden sm:flex flex-col left-28 absolute cursor-pointer'>
+					<p className='text-xl font-medium'>{data.name || ''}</p>
 					<span className='text-xs text-gray-400'>{data?.designation}</span>
 				</div>
 			</div>
 			<Pill />
 			<span className='flex gap-1'>
-				<Link target='_blank' to={data.gitHubLink}>
+				<Link
+					target='_blank'
+					className='hover:text-yellow-300'
+					to={data.gitHubLink}
+				>
 					<FaGithub size='28' />
 				</Link>
-				<Link target='_blank' to={data.linkedInLink}>
+				<Link
+					target='_blank'
+					className='hover:text-yellow-300'
+					to={data.linkedInLink}
+				>
 					<FaLinkedin size='28' />
 				</Link>
 			</span>
