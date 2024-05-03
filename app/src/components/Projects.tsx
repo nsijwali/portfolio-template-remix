9ff97649cafac3f32164510212fc8c953b1a38b8
Nitin Sijwali <NITIN.SIJWALI@questdot.io>
2024-05-03 18:22:29 +0530

    added projects

--- app/src/components/Projects.tsx
+++ app/src/components/Projects.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import { BsArrowDown } from 'react-icons/bs';
-import { Chrome, Dot } from './component.styles';
+import { BsArrowRight } from 'react-icons/bs';
+import { Chrome } from './component.styles';
 import { isMobileDevice } from '~/utils/utils';
 
 const Projects = ({ project }: any) => {
@@ -11,7 +11,7 @@ const Projects = ({ project }: any) => {
 	useEffect(() => {
 		setIsMobile(isMobileDevice());
 	}, [isMobileDevice]);
-	// const size = useMemo(() => (isMobile ? '28' : '40'), [isMobile]);
+	const size = useMemo(() => (isMobile ? '28' : '40'), [isMobile]);
 
 	return project?.map(
 		({ pName, imageUrl, description, responsibilities }: any) => (
@@ -20,8 +20,11 @@ const Projects = ({ project }: any) => {
 				className='w-full sm:w-8/12 max-w-screen-2xl relative overflow-hidden rounded-3xl p-1 sm:p-2 xs:p-2 sm:h-96 xs:h-96 h-72 '
 			>
 				<div className='window-main project p-6 rounded-2xl overflow-hidden h-full relative flex flex-col gap-3 cursor-pointer'>
-					<div className='font-semibold text-base sm:text-xl'>{pName}</div>
-					<div className='text-sm text-gray-400 font-extralight'>
+					<div className='flex justify-between'>
+						<div className='font-semibold text-base sm:text-xl'>{pName}</div>
+						<BsArrowRight className='' size={size} />
+					</div>
+					<div className='sm:text-base text-sm text-gray-400 font-extralight'>
 						{description}
 					</div>
 					<div className='flex justify-center w-full '>
