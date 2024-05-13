e4380e8eb29dee1dd5381a22a75cbcce84ff0ffc
Nitin Sijwali <NITIN.SIJWALI@questdot.io>
2024-05-13 19:48:14 +0530

    chat added

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { vercelPreset } from '@vercel/remix/vite';
 
 export default defineConfig({
-	plugins: [remix({ presets: [vercelPreset()] }), tsconfigPaths()],
+	plugins: [remix(), tsconfigPaths()],
 	experimental: {
 		nftTracing: true,
 	},
