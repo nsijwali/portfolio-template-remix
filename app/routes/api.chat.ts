929e668ce25b36346714561574c2c0678405c707
Nitin Sijwali <NITIN.SIJWALI@questdot.io>
2024-05-13 15:00:18 +0530

    chat added

--- app/routes/api.chat.ts
+++ app/routes/api.chat.ts
@@ -1,6 +1,6 @@
 // import () from '@remix-run/node';
 
-import { getChatStream } from '../utils/chat.server';
+import { getChatStream } from '../../api/chat.server';
 
 export const action = async ({ request }: any) => {
 	return await getChatStream(request);
