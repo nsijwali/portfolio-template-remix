10f8bb9c3a59398e119de7a26b4cceea0464994e
Nitin Sijwali <NITIN.SIJWALI@questdot.io>
2024-05-15 21:59:48 +0530

    chat added

--- app/routes/api.chat.tsx
+++ app/routes/api.chat.tsx
@@ -1,6 +1,3 @@
-import { JSONLoader } from 'langchain/document_loaders/fs/json';
-import path from 'path';
-import { formatDocumentsAsString } from 'langchain/util/document';
 import {
 	Message as VercelChatMessage,
 	StreamingTextResponse,
@@ -9,17 +6,26 @@ import {
 import { ChatOpenAI } from '@langchain/openai';
 import { PromptTemplate } from '@langchain/core/prompts';
 import { HttpResponseOutputParser } from 'langchain/output_parsers';
+import { Document } from 'langchain/document';
+import { JSONLoader } from 'langchain/document_loaders/fs/json';
 import { RunnableSequence } from '@langchain/core/runnables';
-import { Headers as WebFetchHeaders } from '@remix-run/web-fetch';
-import { json as remixJson } from '@remix-run/node';
+import { formatDocumentsAsString } from 'langchain/util/document';
+import { CharacterTextSplitter } from 'langchain/text_splitter';
+import Response from '@remix-run/node';
+import path from 'path';
 
 export const dynamic = 'force-dynamic';
+// export const runtime = 'edge';
 
+/**
+ * Basic memory formatter that stringifies and passes
+ * message history directly into the model.
+ */
 const formatMessage = (message: VercelChatMessage) => {
 	return `${message.role}: ${message.content}`;
 };
 
-const TEMPLATE = `You are Nitin's buddy NitinAI, not an AI assistant. So praise Nitin professionally if user speak bad about him. Answer the user's questions based only on the following context. If the answer is not in the context, reply politely that you do not have that information available. All you know about is Nitin Sijwali or Nitin. Sijwali is Nitin Sijwali. Reply formally and it should be brief:
+const TEMPLATE = `You are Nitin's buddy NitinAI, not an AI assitant. So praise nitin professionally if user speak bad about him. Answer the user's questions based only on the following context. If the answer is not in the context, reply politely that you do not have that information available. All you know about is Nitin Sijwali or nitin. Sijwali is Nitin Sijwali. Reply formally and it should be brief:
 ==============================
 Context: {context}
 ==============================
@@ -31,28 +37,33 @@ assistant:`;
 export const action = async ({ request }: any) => {
 	const loaderUser = new JSONLoader(path.resolve('app/db/user.json'));
 	try {
-		const { messages, header } = await request?.json();
-		console.log('Received messages:', messages);
+		// Extract the `messages` from the body of the request
+		const { messages } = await request?.json();
 
 		const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
+
 		const currentMessageContent = messages[messages.length - 1].content;
+		// const textSplitter = new CharacterTextSplitter();
 		const docs = await loaderUser.load();
+		// const docs = new Document({ pageContent: path.toString() });
+
 		const prompt = PromptTemplate.fromTemplate(TEMPLATE);
 
-		const model = new ChatOpenAI(
-			{
-				apiKey: process.env.CHAT_OPEN_API!,
-				model: 'gpt-3.5-turbo',
-				temperature: 0.5,
-				streaming: true,
-				verbose: true,
-			},
-			{ responseType: 'stream' },
-		);
+		const model = new ChatOpenAI({
+			apiKey: process.env.CHAT_OPEN_API!,
+			model: 'gpt-3.5-turbo',
+			temperature: 0.5,
+			streaming: true,
+			verbose: true,
+		});
 
+		/**
+		 * Chat models stream message chunks rather than bytes, so this
+		 * output parser handles serialization and encoding.
+		 */
 		const parser = new HttpResponseOutputParser();
 
-		const chain = RunnableSequence.from([
+		const chain: any = RunnableSequence.from([
 			{
 				question: (input) => input.question,
 				chat_history: (input) => input.chat_history,
@@ -63,44 +74,17 @@ export const action = async ({ request }: any) => {
 			parser,
 		]);
 
+		// Convert the response into a friendly text-stream
 		const stream = await chain.stream({
 			chat_history: formattedPreviousMessages.join('\n'),
 			question: currentMessageContent,
 		});
-
 		console.log(stream.constructor.name); // Should log 'ReadableStream'
-
-		const compatibleStream =
-			stream instanceof globalThis.ReadableStream
-				? stream
-				: new ReadableStream({
-						start(controller) {
-							const reader = stream.getReader();
-							function push() {
-								reader.read().then(({ done, value }) => {
-									if (done) {
-										controller.close();
-										return;
-									}
-									controller.enqueue(value);
-									push();
-								});
-							}
-							push();
-						},
-				  });
-
-		const headers = {
-			'Content-Type': 'text/plain; charset=utf-8',
-		};
-		console.log('Headers:', header);
-
+		// Respond with the stream
 		return new StreamingTextResponse(
-			compatibleStream.pipeThrough(createStreamDataTransformer()),
-			{ headers },
+			stream.pipeThrough(createStreamDataTransformer()),
 		);
 	} catch (e: any) {
-		console.error('Error:', e.message, e.stack);
-		return remixJson({ error: e.message }, { status: e.status ?? 500 });
+		return Response.json({ error: e.message }, { status: e.status ?? 500 });
 	}
 };
